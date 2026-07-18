f3cae3cb295e355a807ba2dc092dc3146137b9a2
QuyDatSadBoy <dattqyl09@gmail.com>
2026-07-18 22:47:58 +0700

    chore(hero): bỏ import dynamic thừa sau khi gỡ WebGL

--- fe/components/sections/hero.tsx
+++ fe/components/sections/hero.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useRef, useState, type CSSProperties } from "react";
-import dynamic from "next/dynamic";
 import Link from "next/link";
 import Image from "next/image";
 import { motion, useReducedMotion, useScroll, useTransform } from "framer-motion";
